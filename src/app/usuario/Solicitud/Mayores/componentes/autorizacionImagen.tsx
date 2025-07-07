c1d0bcf1788027b011b0fd0b8e177dff74e09ab3
Korosensee <cet.casajardin@gmail.com>
2025-07-07 15:17:20 -0300

    un cambio xd

--- src/app/usuario/Solicitud/Mayores/componentes/autorizacionImagen.tsx
+++ src/app/usuario/Solicitud/Mayores/componentes/autorizacionImagen.tsx
@@ -19,16 +19,14 @@ const AutorizacionImg: React.FC<Datos> = ({ setDatosAutorizacionImage }) => {
             <div className='flex flex-col '>
                 <h1 className='flex justify-center  font-bold text-2xl'>Autorización para uso de Imagen</h1>
                 <div className='flex  justify-center lg:w-1/2 md:1/2 sm:w-2/3 mx-auto mt-5 max-h-[45vh] overflow-y-auto border border-gray-300 rounded-lg p-4 shadow-sm'>
-                    <p className='mx-auto w-full md:'>Por medio de la presente, autorizo al Centro Educativo 
-                        y Terapéutico “Casa Jardín”, a utilizar de manera amplia, irrestricta y gratuita las imágenes
-                        CUIDADAS del/la menor, que sean tomadas en las Actividades del mismo,
-                        con la promoción, difusión y conocimiento de sus actividades.
-                        <br />Renuncio expresa e irrevocablemente a todo y cualquier tipo de
-                        reclamo contra el CET, sus directores, autoridades y miembros en relación
-                        a los derechos autorizados por medio de la presente sobre las Imágenes y/o
-                        a percibir cualquier suma en concepto de indemnización o remuneración por
-                        los usos autorizados precedentemente indicados, relevando al CET de cualquier
-                        responsabilidad al respecto.</p>
+                    <p className='mx-auto w-full md:'>Por medio de la presente, Autorizo al Centro Educativo y Terapéutico “Casa Jardín”, a utilizar de manera amplia, irrestricta
+                        y gratuita las imágenes CUIDADAS en las que apareciera, en las actividades del mismo, con la
+                        promoción difusión y conocimiento de sus actividades
+                        <br />Renuncio expresa e irrevocablemente a todo y cualquier tipo de reclamo contra CET, sus
+                        directores, autoridades y miembros en relación a los derechos autorizados por medio de la
+                        presente sobre las imágenes y/o percibir cualquier suma en concepto de indemnización o
+                        remuneración por los usos autorizados precedentemente indicados, relevando a CET de
+                        cualquier responsabilidad al respecto. </p>
                 </div>
                 <div className='flex justify-center mt-4'>
                     <label className='flex items-center'>
